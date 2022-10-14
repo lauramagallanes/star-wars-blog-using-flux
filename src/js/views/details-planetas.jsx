20b1c41023520f69aebaeeca1445ed696a45d79a
lauramagallanes <pasocenturiontours@gmail.com>
2022-10-14 11:42:24 +0000

    agregue cambios de colores al details-planetasjsx

--- src/js/views/details-planetas.jsx
+++ src/js/views/details-planetas.jsx
@@ -17,29 +17,29 @@ export const DetailsPlanetas = props => {
 
 
 			{/* card */}
-			<div className="card mb-3" style={{ maxWidth: "80%", margin: "auto" }}>
+			<div className="card mb-3" style={{ maxWidth: "80%", margin: "auto", background: "#282727" }}>
 				<div className="row g-0">
 					<div className="col-md-4">
 						<img src={"https://starwars-visualguide.com/assets/img/planets/" + (params.id) + ".jpg"} className="img-fluid rounded-start" alt="..." />
 					</div>
 					<div className="col-md-8">
 						<div className="card-body">
-							<h2 className="card-title">{store.detallesPlaneta?.name}</h2>
-							<p className="card-text">orem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus lobortis mi ut tempor. Curabitur ultrices dapibus nisl. Sed dictum tempor ligula, eget varius enim dignissim eu. Maecenas ut sapien sagittis odio elementum eleifend convallis sollicitudin erat. Praesent finibus ligula turpis, ac placerat enim euismod ut. Maecenas laoreet dolor leo, ut ultricies ex ultricies vel.</p>
-							<p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+							<h2 className="card-title text-light">{store.detallesPlaneta?.name}</h2>
+							<p className="card-text text-light">orem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus lobortis mi ut tempor. Curabitur ultrices dapibus nisl. Sed dictum tempor ligula, eget varius enim dignissim eu. Maecenas ut sapien sagittis odio elementum eleifend convallis sollicitudin erat. Praesent finibus ligula turpis, ac placerat enim euismod ut. Maecenas laoreet dolor leo, ut ultricies ex ultricies vel.</p>
+							<p className="card-text text-light"><small className="text-muted">Last updated 3 mins ago</small></p>
 						</div>
 					</div>
 				</div>
 				<div className="container text-center">
 					<div className="row row-cols-6">
-						<div className="col text-danger"><h5>Name</h5><h5>{store.detallesPlaneta?.name}</h5></div>
-						<div className="col text-danger"><h5>Population</h5><h5>{store.detallesPlaneta?.population
+						<div className="col text-light"><h5>Name</h5><h5>{store.detallesPlaneta?.name}</h5></div>
+						<div className="col text-light"><h5>Population</h5><h5>{store.detallesPlaneta?.population
 						}</h5></div>
-						<div className="col text-danger"><h5>Rotation Period</h5><h5>{store.detallesPlaneta?.rotation_period}</h5></div>
-						<div className="col text-danger"><h5>Surface Water</h5><h5>{store.detallesPlaneta?.surface_water}</h5></div>
-						<div className="col text-danger"><h5>Gravity</h5><h5>{store.detallesPlaneta?.gravity
+						<div className="col text-light"><h5>Rotation Period</h5><h5>{store.detallesPlaneta?.rotation_period}</h5></div>
+						<div className="col text-light"><h5>Surface Water</h5><h5>{store.detallesPlaneta?.surface_water}</h5></div>
+						<div className="col text-light"><h5>Gravity</h5><h5>{store.detallesPlaneta?.gravity
 						}</h5></div>
-						<div className="col text-danger"><h5>Climate</h5><h5>{store.detallesPlaneta?.climate}</h5></div>
+						<div className="col text-light"><h5>Climate</h5><h5>{store.detallesPlaneta?.climate}</h5></div>
 					</div>
 				</div>
 			</div>
